16f30daab69d41fb60658b3457206f52e0984938
srivatsa damn <srivatsarog@gmail.com>
2025-02-18 23:57:18 +0530

    final code

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -9,7 +11,49 @@ import (
 	"github.com/srivatsa-bot/gomon/watcher"
 )
 
+var Version string = "1.0.0"
+
 func main() {
+
+	//cli kit
+	fileName := flag.String("file", "server.go", "File to watch and run(Handles only one file)")
+	showVersion := flag.Bool("version", false, "Show version information")
+	showHelp := flag.Bool("help", false, "Show help information")
+
+	flag.Parse() //takes input from cli and assigns it flags
+
+	// Show version and exit
+	if *showVersion {
+		fmt.Printf("gomon version %s\n", Version)
+		return
+	}
+	// Show help and exit
+	if *showHelp {
+		fmt.Println("Usage of gomon:")
+		fmt.Println("  gomon [flags] [file]")
+		fmt.Println("\nFlags:")
+		flag.PrintDefaults() //prints all the default flags
+		return
+	}
+
+	//get filename form tag
+	targetFile := *fileName
+	//to make gomon server.go(nonflag argument work)
+	if flag.NArg() > 0 {
+		targetFile = flag.Arg(0) //first non flag argument
+
+		//if user enters more than one non flag argument
+		if flag.NArg() > 1 {
+			logger.Info("Warning: Multiple files provided. Only watching %s", *fileName)
+		}
+	}
+
+	//check if file exists then proceed
+	if _, err := os.Stat(targetFile); os.IsNotExist(err) { //os.isnotexist returns true if file doesnt exist
+		fmt.Printf("File %s does not exist\n", targetFile)
+		os.Exit(1)
+	}
+
 	//siganl handling
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan,
@@ -20,7 +64,7 @@ func main() {
 	)
 
 	//get the file stat
-	fw, err := watcher.NewFileWatcher("server.go")
+	fw, err := watcher.NewFileWatcher(targetFile)
 	if err != nil {
 		logger.Error("Failed to create file watcher: %v", err)
 		return
@@ -42,7 +86,7 @@ func main() {
 		sig := <-sigChan
 		logger.Info("\nReceived %v signal. Shutting down...", sig)
 		fw.Cleanup()
-		os.Exit(0)
+		os.Exit(0) //exit sucesfully
 
 	}()
 
